b58e44b13ba33d4d9b3e6ac01f4a8a8394ee3498
Philippe Moneda <philippemoneda@gmail.com>
2018-06-07 18:52:24 -0300

    quando criar a solution já cria o root dos repositórios

--- actions/apps/solution/create_solution.go
+++ actions/apps/solution/create_solution.go
@@ -2,7 +2,9 @@ package solution
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/ONSBR/Plataforma-Deployer/env"
 	"github.com/ONSBR/Plataforma-Deployer/models"
 	"github.com/ONSBR/Plataforma-Deployer/models/exceptions"
 	"github.com/ONSBR/Plataforma-Deployer/sdk/apicore"
@@ -10,9 +12,26 @@ import (
 
 //CreateSolution on platform
 func CreateSolution(solution *models.Solution) *exceptions.Exception {
+	ex := createSolutionOnAPICore(solution)
+	if ex != nil {
+		return ex
+	}
+	return createRootFolderForSolutionOnGit(solution)
+}
+
+func createSolutionOnAPICore(solution *models.Solution) *exceptions.Exception {
 	sol := FindSolutionByID(solution.ID)
 	if sol != nil {
 		return exceptions.NewInvalidArgumentException(fmt.Errorf("solution %s already exists", solution.ID))
 	}
 	return apicore.PersistOne(solution)
+}
+
+func createRootFolderForSolutionOnGit(solution *models.Solution) *exceptions.Exception {
+	path := fmt.Sprintf("%s/%s", env.GetGitServerReposPath(), solution.Name)
+	err := os.Mkdir(path, 0777)
+	if err != nil {
+		return exceptions.NewComponentException(err)
+	}
+	return nil
 }
