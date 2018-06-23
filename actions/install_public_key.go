6c294a5a4e1bd6f78917ad4e92502438dbad9315
Philippe Moneda <philippemoneda@gmail.com>
2018-06-23 11:50:12 -0300

    restarta o container git server para que a chave publica fique habilitada

--- actions/install_public_key.go
+++ actions/install_public_key.go
@@ -3,10 +3,12 @@ package actions
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/ONSBR/Plataforma-Deployer/env"
 	"github.com/ONSBR/Plataforma-Deployer/models"
 	"github.com/ONSBR/Plataforma-Deployer/models/exceptions"
+	"github.com/PMoneda/whaler"
 )
 
 //InstallPublicKey install user's public key on git-server
@@ -23,7 +25,10 @@ func InstallPublicKey(content []byte, solution, keyName string) (*models.PublicK
 	info := new(models.PublicKeyInfo)
 	info.Name = keyName
 	info.Solution = solution
+	timeout := 60 * time.Second
+	if err := whaler.RestartContainer("/git-server", &timeout); err != nil {
+		return nil, exceptions.NewComponentException(err)
+	}
 	info.Ok()
-
 	return info, nil
 }
